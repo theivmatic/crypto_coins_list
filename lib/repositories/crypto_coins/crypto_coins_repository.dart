bc1a9470307b74ab5f2a91fb30e3adb04c7bf8ee
theivmatic <theivmatic@mail.ru>
2023-10-12 09:46:21 +0500

    made GetIt and registred it

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -2,9 +2,15 @@ import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 
 class CryptoCoinsRepository implements AbstractCoinsRepository {
+  CryptoCoinsRepository({
+    required this.dio,
+  });
+
+  final Dio dio;
+
   @override
   Future<List<CryptoCoin>> getCoinsList() async {
-    final response = await Dio().get(
+    final response = await dio.get(
         'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,SOL,CAG,DOV&tsyms=USD');
 
     final data = response.data as Map<String, dynamic>;
